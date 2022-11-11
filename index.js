06c70c4b52dd10ee3ec7ecd36c3624d18420015d
yuilovestomato <129kjy@gmail.com>
2022-11-11 07:35:06 +0000

    added bootstrap: tried to use npm but with html

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+//import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
+
 app.listen(8080, function() {
 	console.log('listening on 8080')
 })
 
 app.get('/', function(req, res) {
-	res.send('sonyometal');
+	res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/subpage', function(req, res) {
