125f2d679a0e42851a68ce8b08e70448fdda3d6b
yuilovestomato <129kjy@gmail.com>
2022-12-17 11:22:47 +0000

    changed write.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser')
+
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
@@ -23,39 +24,32 @@ mysqlDB.connect();
 //at server start
 //post list
 app.listen(8080, function() {
-	console.log('listening on 8080')
+        console.log('listening on 8080')
 })
 
 //go to home
 app.get('/', function(req, res) {
-	mysqlDB.query('SELECT * FROM posts', function (req, result) {
-		res.render('index', { info : result });
-	});
+        mysqlDB.query('SELECT * FROM posts', function (req, result) {
+                res.render('index', { info : result });
+        });
 })
 
-//list ejs
-app.get('/list', function(req, res) {
-	mysqlDB.query('SELECT * FROM posts', function (req, result) {
-		res.render('index', { info : result });
-	});
-});
-
-//writing some shit
+//writing notices
 app.get('/write', function(req, res) {
-	res.sendFile(__dirname + '/write.html');
+        res.sendFile(__dirname + '/write.html');
 })
 
 //posting process
 app.post('/add', function(req, res) {
-	var id = req.body.id;
-	var password = req.body.password;
-	var title = req.body.title;
-	var description = req.body.description;
+        var id = req.body.id;
+        var password = req.body.password;
+        var title = req.body.title;
+        var description = req.body.description;
 
-	var sql = `INSERT INTO posts (id, password, title, post) VALUES ("${id}", "${password}", "${title}", "${description}")`;
-	mysqlDB.query(sql, function(err, result) {
-		if (err) throw err;
-		console.log('record inserted');
-		res.redirect('/');
-	});
+        var sql = `INSERT INTO posts (id, password, title, post) VALUES ("${id}", "${password}", "${title}", "${description}")`;
+        mysqlDB.query(sql, function(err, result) {
+                if (err) throw err;
+                console.log('record inserted');
+                res.redirect('/');
+        });
 });
